dc1f7e88703498f36c9c22cb61f65470074e45a1
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2025-04-23 18:33:33 +0700

    first commit

---
+++ tugas4/android/app/src/main/kotlin/com/example/tugas4/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tugas4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ tugas4/lib/register.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
