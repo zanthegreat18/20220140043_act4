513bca19d24a95e476af2e5760da3867932a7ecd
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2025-04-23 19:00:04 +0700

    order makanan dan minuman dan kalkulasi jumlah

--- tugas4/lib/order.dart
+++ tugas4/lib/order.dart
@@ -7,3 +7,19 @@ class OrderPage extends StatefulWidget {
   @override
   State<OrderPage> createState() => _OrderPageState();
 }
+
+class _OrderPageState extends State<OrderPage> {
+  final TextEditingController makananController = TextEditingController();
+  final TextEditingController minumanController = TextEditingController();
+  final TextEditingController jumlahMakananController = TextEditingController();
+  final TextEditingController jumlahMinumanController = TextEditingController();
+  int totalHarga = 0;
+
+  void calculateTotalPrice() {
+    int jumlahMakanan = int.tryParse(jumlahMakananController.text) ?? 0;
+    int jumlahMinuman = int.tryParse(jumlahMinumanController.text) ?? 0;
+
+    setState(() {
+      totalHarga = (jumlahMakanan * 32000) + (jumlahMinuman * 5000);
+    });
+  }
