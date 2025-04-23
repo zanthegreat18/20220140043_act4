4fd1c5a47c72641caa7874ab90ac8539226ad03e
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2025-04-23 18:58:53 +0700

    import order dan deklarasi order page

---
+++ tugas4/lib/order.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:play_navigation/presentation/detail_order_page.dart';
+
+class OrderPage extends StatefulWidget {
+  const OrderPage({super.key});
+
+  @override
+  State<OrderPage> createState() => _OrderPageState();
+}
