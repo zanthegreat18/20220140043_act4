a566e4531dbf8940e7a38ba19378cc1147985be7
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2025-04-23 19:07:35 +0700

    menampilkan pemesanan user

---
+++ tugas4/lib/detail_order_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:play_navigation/presentation/home_page.dart';
+
+class DetailOrderPage extends StatelessWidget {
+  final String makanan;
+  final String minuman;
+  final String jumlahMakanan;
+  final String jumlahMinuman;
+  final int totalHarga;
+
+  const DetailOrderPage({
+    super.key,
+    required this.makanan,
+    required this.minuman,
+    required this.jumlahMakanan,
+    required this.jumlahMinuman,
+    required this.totalHarga,
+  });
