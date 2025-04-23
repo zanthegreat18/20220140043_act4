bdb5d53cd154f0958d288f6df48a054b6aa67480
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2025-04-23 19:08:29 +0700

    menampilkan rincian makanan

--- tugas4/lib/detail_order_page.dart
+++ tugas4/lib/detail_order_page.dart
@@ -16,3 +16,35 @@ class DetailOrderPage extends StatelessWidget {
     required this.jumlahMinuman,
     required this.totalHarga,
   });
+
+   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Detail Order Page')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Food Order: $makanan'),
+            Text('Drink Order: $minuman'),
+            Text('Food QTY Order: $jumlahMakanan'),
+            Text('Drink QTY Order: $jumlahMinuman'),
+            Text('Total Price: $totalHarga'),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                  (route) => false,
+                );
+              },
+              child: Text('Finish Order'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
