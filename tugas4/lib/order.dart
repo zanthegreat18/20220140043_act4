2702141c8c05b91701acd4d184218acabac43681
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2025-04-23 19:02:36 +0700

    tombol navigasi

--- tugas4/lib/order.dart
+++ tugas4/lib/order.dart
@@ -74,3 +74,29 @@ class _OrderPageState extends State<OrderPage> {
                 return null;
               },
             ),
+             ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  calculateTotalPrice();
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DetailOrderPage(
+                        jumlahMakanan: jumlahMakananController.text,
+                        jumlahMinuman: jumlahMinumanController.text,
+                        makanan: makananController.text,
+                        minuman: minumanController.text,
+                        totalHarga: totalHarga,
+                      ),
+                    ),
+                  );
+                }
+              },
+              child: Text('Order Now'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
