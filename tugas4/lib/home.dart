2e6e5afc8e6976ddf14aa3a252be63aaeee89d10
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2025-04-23 18:49:56 +0700

    membuiat method untuk home page

--- tugas4/lib/home.dart
+++ tugas4/lib/home.dart
@@ -7,3 +7,21 @@ class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() => _HomePageState();
 }
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Page')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Selamat datang di Home Page!'),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const OrderPage()),
+                );
+              },
